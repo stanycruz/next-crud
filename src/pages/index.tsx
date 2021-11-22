18bed3fb99ecefa7838f694a28f70064c9ee37ab
Stanislaw Cruz <stanycruz@outlook.com>
2021-11-22 16:09:25 -0300

    Componente Tabela #01

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,15 @@
 import Layout from '../components/Layout'
+import Tabela from '../components/Tabela'
+import Cliente from '../core/Cliente'
 
 export default function Home() {
+
+  const clientes = [
+    new Cliente('Sarah Sara Viana', 77, '1'),
+    new Cliente('Paulo Erick Freitas', 63, '2'),
+    new Cliente('Edson Kevin Miguel Bernardes', 21, '3'),
+    new Cliente('Murilo Kauê Pinto', 59, '4'),
+  ]
   return (
     <div className={`
       flex justify-center items-center h-screen
@@ -8,7 +17,7 @@ export default function Home() {
       text-white
     `}>
       <Layout titulo="Cadastro Simples">
-        <span>Conteudo</span>
+        <Tabela clientes={clientes}></Tabela>
       </Layout>
     </div>
   )
